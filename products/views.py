d64373e80717a2a3ca412758d6df229ed3b7d89c
Karin Oldbring <karin.oldbring@gmail.com>
2022-06-27 08:55:24 +0000

    Create view for product_detail

--- products/views.py
+++ products/views.py
@@ -13,3 +13,17 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """
+    View to see products in detail
+    """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
