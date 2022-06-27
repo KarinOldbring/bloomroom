5d98b8e8d7ffbb21ba65de0b6aad554a7c00b30b
Karin Oldbring <karin.oldbring@gmail.com>
2022-06-27 14:12:25 +0000

    Add links for categories at base.html

--- products/views.py
+++ products/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from django.db.models import Q
-from .models import Product
+from .models import Product, Category
 
 def all_products(request):
     """
@@ -10,8 +10,18 @@ def all_products(request):
 
     products = Product.objects.all()
     query = None
+    categories = None
 
     if request.GET:
+
+        if 'category' in request.GET:
+            categories = request.GET['category'].split(',')
+            print(categories)
+            products = products.filter(category__friendly_name__in=categories)
+            print(products)
+            categories = Category.objects.filter(friendly_name__in=categories)
+            print(categories)
+
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -24,6 +34,7 @@ def all_products(request):
     context = {
         'products': products,
         'search_term': query,
+        'current_categories': categories,
     }
 
     return render(request, 'products/products.html', context)
