3df04d3bce209c8ecff28f42538429e7bf410e3b
Karin Oldbring <karin.oldbring@gmail.com>
2022-06-28 18:44:05 +0000

    Add url path for about

--- home/views.py
+++ home/views.py
@@ -7,3 +7,10 @@ def index(request):
     View to return index page
     """
     return render(request, 'home/index.html')
+
+
+def about(request):
+    """
+    View to return about page
+    """
+    return render(request, 'home/about.html')
