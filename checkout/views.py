1f5d9d4c363dbb2e94354e0dab2c73c5e37c0103
Karin Oldbring <karin.oldbring@gmail.com>
2022-07-01 16:09:44 +0000

    Add noimage-image

--- checkout/views.py
+++ checkout/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
+from django.contrib import messages
 
-# Create your views here.
+from .forms import OrderForm
+
+def checkout(request):
+    """
+    Prevents entering checkout with empty bag
+    """
+    bag = request.session.get('bag', {})
+    if not bag:
+        messages.error(request, 'Your shopping bag is empty at the moment!')
+        return redirect(reverse('products'))
+
+    order_form = OrderForm()
+    template = 'checkout/checkout.html'
+    context = {
+        'order_form': order_form
+    }
+
+    return render(request, template, context)
