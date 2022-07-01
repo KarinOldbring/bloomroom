0ef46432bad83eff12bb43d1569738ee57f782f1
Karin Oldbring <karin.oldbring@gmail.com>
2022-07-01 08:50:26 +0000

    Add toasts

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.contrib import messages
+from products.models import Product
 
 
 def view_bag(request):
@@ -12,14 +14,17 @@ def add_to_bag(request, item_id):
     """
     Add quantity to shopping bag
     """
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     bag = request.session.get('bag', {})
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity!')
     else:
         bag[item_id] = quantity
+        messages.success(request, f'{product.name} was added to your bag!')
     
     request.session['bag'] = bag
     return redirect(redirect_url)
@@ -29,13 +34,16 @@ def adjust_bag(request, item_id):
     """
     Change quantity shopping bag
     """
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     bag = request.session.get('bag', {})
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity!')
     else:
         bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from shopping bag')
     
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -46,11 +54,14 @@ def remove_from_bag(request, item_id):
    Remove items from shopping bag
     """
     try:
-        bag.pop(item_id)
+        product = get_object_or_404(Product, pk=item_id)
         bag = request.session.get('bag', {})
+        bag.pop(item_id)
+        messages.success(request, f'Removed {product.name} from your bag')
     
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
