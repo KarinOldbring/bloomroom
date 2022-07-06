7a89282f3e35d3db08d19b8446f78336871c88b9
Karin Oldbring <karin.oldbring@gmail.com>
2022-07-06 18:40:49 +0000

    Add season to products model, update template

--- products/models.py
+++ products/models.py
@@ -28,6 +28,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     price = models.DecimalField(max_digits=6, decimal_places=2)
     description = models.TextField()
+    season = models.CharField(max_length=254, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
