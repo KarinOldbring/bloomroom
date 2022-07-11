cf8a3e153a27c3c008500eaafdb6c543c33a31b6
Karin Oldbring <karin.oldbring@gmail.com>
2022-07-11 14:11:23 +0000

    Add if not debug in settings

--- bloomroom/settings.py
+++ bloomroom/settings.py
@@ -58,7 +58,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -185,6 +184,10 @@ STATIC_URL = '/static/'
 STATICFILES_STORAGE = (
     'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 )
+
+if not DEBUG:
+    MEDIA_URL = '/BloomRoom/media/'
+    STATIC_URL = '/BloomRoom/static/'
 
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'), ]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
