4aa8c16cb725abe87819de793cd037a5d6053b86
Karin Oldbring <karin.oldbring@gmail.com>
2022-07-12 16:48:31 +0000

    Create new bucket and change settings

--- bloomroom/settings.py
+++ bloomroom/settings.py
@@ -185,7 +185,7 @@ MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 if 'USE_AWS' in os.environ:
-    AWS_STORAGE_BUCKET_NAME = 'bloomroom.herokuapp.com'
+    AWS_STORAGE_BUCKET_NAME = 'bloomroom'
     AWS_S3_REGION_NAME = 'eu-north-1'
     AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
     AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
